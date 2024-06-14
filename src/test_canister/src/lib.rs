3dec32d38bc26a04c646870b4fdb7410c29f2571
tiamo <vk.tiamo@gmail.com>
2024-06-14 22:14:08 +0300

    test: add transfer sol integration test

--- src/test_canister/src/lib.rs
+++ src/test_canister/src/lib.rs
@@ -1,53 +1,96 @@
+use candid::{CandidType, Principal};
+use ic_solana::rpc_client::RpcResult;
+use ic_solana::types::{AccountMeta, BlockHash, Instruction, Pubkey};
 use std::cell::RefCell;
-
-use candid::Principal;
-use ic_cdk::{init, query};
-
-type GetAddressResponse = Result<String, String>;
+use std::str::FromStr;
 
 thread_local! {
     static SOL_PROVIDER_CANISTER: RefCell<Option<Principal>>  = const { RefCell::new(None) };
 }
 
-/// The whoami method returns the calling principal and the sol address of the caller. A prerequisite
-/// for a successful response is that the test canister has been initialized with a reference to the siws
-/// provider canister and that the caller has completed the authentication process with the siws
-/// provider canister.
-#[query]
-async fn whoami() -> Result<(String, String), String> {
-    let calling_principal = ic_cdk::caller();
+fn sol_canister_id() -> Principal {
+    SOL_PROVIDER_CANISTER
+        .with_borrow(|canister| canister.expect("Solana provider canister not initialized"))
+}
 
-    // Get the sol provider canister reference
-    let sol_provider_canister = SOL_PROVIDER_CANISTER
-        .with_borrow(|canister| canister.expect("Solana provider canister not initialized"));
+#[derive(CandidType, Debug)]
+pub struct SendTransactionRequest {
+    pub instructions: Vec<String>,
+    pub recent_blockhash: Option<String>,
+}
 
-    let response: Result<(GetAddressResponse,), _> = ic_cdk::call(
-        sol_provider_canister,
-        "get_address",
-        (calling_principal.as_slice(),),
+#[ic_cdk::update]
+async fn test() {
+    let sol_canister = sol_canister_id();
+
+    // Get the solana address associated with the caller
+    let response: Result<(String,), _> = ic_cdk::call(sol_canister, "get_address", ()).await;
+    let solana_address = Pubkey::from_str(&response.unwrap().0).unwrap();
+    ic_cdk::println!("solana_address: {}", solana_address);
+
+    // Get the balance
+    let response: Result<(RpcResult<u64>,), _> = ic_cdk::call(
+        sol_canister,
+        "sol_getBalance",
+        (solana_address.to_string(),),
+    )
+    .await;
+    let lamports = response.unwrap().0.unwrap();
+    ic_cdk::println!("Balance: {} lamports", lamports);
+
+    let fee = 10_000;
+    let amount = 1_000_000u64;
+
+    if lamports <= amount + fee {
+        ic_cdk::trap("Not enough lamports");
+    }
+
+    // Get the latest blockhash
+    let response: Result<(RpcResult<String>,), _> =
+        ic_cdk::call(sol_canister, "sol_latestBlockhash", ()).await;
+    let blockhash = BlockHash::from_str(&response.unwrap().0.unwrap()).unwrap();
+    ic_cdk::println!("Latest Blockhash: {:?}", blockhash);
+
+    // Generate a transfer instruction
+    let system_program_id = Pubkey::from_str("11111111111111111111111111111111").unwrap();
+    let transfer_ix = Instruction::new_with_bincode(
+        system_program_id,
+        &(2, 0, 0, 0, 64, 66, 15, 0, 0, 0, 0, 0), // transfer 1_000_000 lamports
+        vec![
+            AccountMeta::new(solana_address, true),
+            AccountMeta::new(
+                Pubkey::from_str("AAAAUrmaZWvna6vHndc5LoVWUBmnj9sjxnvPz5U3qZGY").unwrap(),
+                false,
+            ),
+            AccountMeta::new(system_program_id, false),
+        ],
+    );
+
+    let response: Result<(RpcResult<String>,), _> = ic_cdk::call(
+        sol_canister,
+        "sol_sendTransaction",
+        (SendTransactionRequest {
+            instructions: vec![transfer_ix.to_string()],
+            recent_blockhash: Some(blockhash.to_string()),
+        },),
     )
     .await;
 
-    let address = match response {
-        Ok(inner_result) => {
-            // Handle the inner Result (GetAddressResponse)
-            match inner_result.0 {
-                Ok(address) => address,  // Successfully got the address
-                Err(e) => return Err(e), // Handle error in GetAddressResponse
-            }
-        }
-        Err(_) => return Err("Failed to get the caller address".to_string()), // Handle ic_cdk::call error
-    };
+    let signature = response.unwrap().0.unwrap();
+    ic_cdk::println!("Signature: {:?}", signature);
 
-    // Return the calling principal and address
-    Ok((calling_principal.to_text(), address))
+    // let transaction: Result<(RpcResult<EncodedConfirmedTransactionWithStatusMeta>,), _> =
+    //     ic_cdk::call(sol_canister, "sol_getTransaction", (signature.to_string(),)).await;
+    //
+    // ic_cdk::println!("Transaction: {:?}", transaction);
 }
 
 /// When setting up the test canister, we need to save a reference to the solana provider canister
 /// so that we can call it later.
-#[init]
-async fn init(c: String) {
+#[ic_cdk::init]
+async fn init(sol_canister: String) {
     SOL_PROVIDER_CANISTER.with(|canister| {
-        *canister.borrow_mut() = Some(Principal::from_text(c).expect("Invalid principal"));
+        *canister.borrow_mut() =
+            Some(Principal::from_text(sol_canister).expect("Invalid principal"));
     });
 }
