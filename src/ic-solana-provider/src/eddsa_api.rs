665f0f9f45d3ea6ce858dbec0a3234d781e1ada8
tiamo <vk.tiamo@gmail.com>
2024-09-11 07:12:04 +0300

    feat: native schnorr support

--- src/ic-solana-provider/src/eddsa_api.rs
+++ src/ic-solana-provider/src/eddsa_api.rs
@@ -1,54 +1,93 @@
-use crate::state::read_state;
-use candid::Principal;
-
-use ic_management_canister_types::{
-    DerivationPath, SchnorrAlgorithm, SchnorrKeyId, SchnorrPublicKeyArgs, SchnorrPublicKeyResponse,
-    SignWithSchnorrArgs, SignWithSchnorrReply,
+use {
+    candid::Principal,
+    ic_management_canister_types::{
+        DerivationPath, SchnorrAlgorithm, SchnorrKeyId, SchnorrPublicKeyArgs,
+        SchnorrPublicKeyResponse, SignWithSchnorrArgs, SignWithSchnorrReply,
+    },
+    serde_bytes::ByteBuf,
+    std::{
+        fmt::{self, Display},
+        str::FromStr,
+    },
 };
-use serde_bytes::ByteBuf;
+
+#[derive(Debug, Clone)]
+pub enum SchnorrKey {
+    TestKeyLocal,
+    TestKey1,
+    ProductionKey1,
+    Custom(String),
+}
+
+impl Display for SchnorrKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // https://internetcomputer.org/docs/current/developer-docs/smart-contracts/signatures/signing-messages-t-schnorr
+        let key_str = match self {
+            SchnorrKey::TestKeyLocal => "dfx_test_key",
+            SchnorrKey::TestKey1 => "test_key_1",
+            SchnorrKey::ProductionKey1 => "key_1",
+            SchnorrKey::Custom(key) => key,
+        };
+        f.write_str(key_str)
+    }
+}
+
+impl FromStr for SchnorrKey {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "dfx_test_key" => Ok(SchnorrKey::TestKeyLocal),
+            "test_key_1" => Ok(SchnorrKey::TestKey1),
+            "key_1" => Ok(SchnorrKey::ProductionKey1),
+            _ => Ok(SchnorrKey::Custom(s.to_string())),
+        }
+    }
+}
 
 /// Fetches the ed25519 public key from the schnorr canister.
-pub async fn eddsa_public_key(key_name: String, derivation_path: Vec<ByteBuf>) -> Vec<u8> {
-    let canister_id = read_state(|s| Principal::from_text(&s.schnorr_canister).unwrap());
-
+pub async fn eddsa_public_key(key: SchnorrKey, derivation_path: Vec<ByteBuf>) -> Vec<u8> {
     let res: Result<(SchnorrPublicKeyResponse,), _> = ic_cdk::call(
-        canister_id,
+        Principal::management_canister(),
         "schnorr_public_key",
         (SchnorrPublicKeyArgs {
             canister_id: None,
             derivation_path: DerivationPath::new(derivation_path),
             key_id: SchnorrKeyId {
                 algorithm: SchnorrAlgorithm::Ed25519,
-                name: key_name,
+                name: key.to_string(),
             },
         },),
     )
     .await;
 
-    res.unwrap().0.public_key
+    res.expect("Failed to fetch ed25519 public key")
+        .0
+        .public_key
 }
 
 /// Signs a message with an ed25519 key.
 pub async fn sign_with_eddsa(
-    key_name: String,
+    key: SchnorrKey,
     derivation_path: Vec<ByteBuf>,
     message: Vec<u8>,
 ) -> Vec<u8> {
-    let canister_id = read_state(|s| Principal::from_text(&s.schnorr_canister).unwrap());
-
     let res: Result<(SignWithSchnorrReply,), _> = ic_cdk::call(
-        canister_id,
+        Principal::management_canister(),
         "sign_with_schnorr",
-        (SignWithSchnorrArgs {
-            message,
-            derivation_path: DerivationPath::new(derivation_path),
-            key_id: SchnorrKeyId {
-                name: key_name,
-                algorithm: SchnorrAlgorithm::Ed25519,
+        (
+            SignWithSchnorrArgs {
+                message,
+                derivation_path: DerivationPath::new(derivation_path),
+                key_id: SchnorrKeyId {
+                    algorithm: SchnorrAlgorithm::Ed25519,
+                    name: key.to_string(),
+                },
             },
-        },),
+            // https://internetcomputer.org/docs/current/references/t-sigs-how-it-works/#fees-for-the-t-schnorr-production-key
+            // 26_153_846_153,
+        ),
     )
     .await;
 
-    res.unwrap().0.signature
+    res.expect("Failed to sign with ed25519").0.signature
 }
