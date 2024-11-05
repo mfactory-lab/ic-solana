5dcf7bc641cd3aa96b0f1f85c035214384b81d7c
tiamo <vk.tiamo@gmail.com>
2024-11-05 04:11:02 +0200

    feat(wallet): add tests

---
+++ src/ic-solana-wallet/tests/setup.rs
@@ -0,0 +1,79 @@
+use std::path::PathBuf;
+
+use candid::{utils::ArgumentEncoder, CandidType, Principal};
+use ic_solana_wallet::state::InitArgs;
+use ic_test_utilities_load_wasm::load_wasm;
+use serde::de::DeserializeOwned;
+use test_utils::{CallFlow, TestSetup};
+
+thread_local! {
+     static RPC_WASM: Vec<u8> = load_wasm(get_root(), "ic-solana-rpc", &[]);
+     static WASM: Vec<u8> = load_wasm(env!("CARGO_MANIFEST_DIR"), env!("CARGO_PKG_NAME"), &[]);
+}
+
+#[derive(Clone)]
+pub struct SolanaWalletSetup {
+    rpc_setup: TestSetup,
+    setup: TestSetup,
+}
+
+impl SolanaWalletSetup {
+    pub fn new() -> Self {
+        let rpc_setup = TestSetup::new(
+            RPC_WASM.with(|wasm| wasm.clone()),
+            ic_solana_rpc::state::InitArgs {
+                demo: Some(true),
+                managers: Some(vec![TestSetup::controller_id()]),
+            },
+        );
+
+        let sol_canister = Some(rpc_setup.canister_id);
+
+        Self {
+            rpc_setup,
+            setup: TestSetup::new(
+                WASM.with(|wasm| wasm.clone()),
+                InitArgs {
+                    sol_canister,
+                    schnorr_key: None,
+                },
+            ),
+        }
+    }
+
+    #[allow(clippy::wrong_self_convention)]
+    pub fn as_controller(mut self) -> Self {
+        self.setup = self.setup.as_controller();
+        self
+    }
+
+    #[allow(clippy::wrong_self_convention)]
+    pub fn as_caller<T: Into<Principal>>(mut self, id: T) -> Self {
+        self.setup = self.setup.as_caller(id);
+        self
+    }
+
+    pub fn upgrade_canister(&self, args: InitArgs) {
+        self.setup.upgrade_canister(WASM.with(|wasm| wasm.clone()), args)
+    }
+
+    pub fn call_update<A: ArgumentEncoder, R: CandidType + DeserializeOwned>(
+        &self,
+        method: &str,
+        args: A,
+    ) -> CallFlow<R> {
+        self.setup.call_update(method, args)
+    }
+
+    pub fn call_query<A: ArgumentEncoder, R: CandidType + DeserializeOwned>(&self, method: &str, args: A) -> R {
+        self.setup.call_query(method, args)
+    }
+}
+
+/// Retrieves the project root directory.
+fn get_root() -> PathBuf {
+    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("../../")
+        .canonicalize()
+        .expect("Invalid project root path")
+}
---
+++ src/ic-solana-wallet/tests/tests.rs
@@ -0,0 +1,43 @@
+use std::str::FromStr;
+
+use ic_solana::{rpc_client::RpcServices, types::Pubkey};
+use test_utils::MockOutcallBuilder;
+
+mod setup;
+
+use crate::setup::SolanaWalletSetup;
+
+#[test]
+fn test_address() {
+    let setup = SolanaWalletSetup::new();
+    let addr = setup.call_update::<_, String>("address", ()).wait();
+    assert_eq!(addr, "FzqpyocjAXKyeRwAt19vnMiGcYN3bc1ZWm56rY1YjYSW");
+    let addr = setup.as_controller().call_update::<_, String>("address", ()).wait();
+    assert_eq!(addr, "G6Po2VMdTwqNuucU8eywanqJ9GyjA6yZcEazNd9prWNu");
+}
+
+#[test]
+fn test_sign_message() {
+    let setup = SolanaWalletSetup::new();
+    let message = "test123";
+    let address = setup.call_update::<_, String>("address", ()).wait();
+    let pubkey = Pubkey::from_str(&address).unwrap();
+    let signature = setup.call_update::<_, Vec<u8>>("signMessage", (message,)).wait();
+    let is_valid = pubkey.verify_signature(message.as_bytes(), &signature);
+    assert!(is_valid)
+}
+
+#[test]
+fn test_send_transaction() {
+    let setup = SolanaWalletSetup::new();
+
+    let raw_tx ="4hXTCkRzt9WyecNzV1XPgCDfGAZzQKNxLXgynz5QDuWWPSAZBZSHptvWRL3BjCvzUXRdKvHL2b7yGrRQcWyaqsaBCncVG7BFggS8w9snUts67BSh3EqKpXLUm5UMHfD7ZBe9GhARjbNQMLJ1QD3Spr6oMTBU6EhdB4RD8CP2xUxr2u3d6fos36PD98XS6oX8TQjLpsMwncs5DAMiD4nNnR8NBfyghGCWvCVifVwvA8B8TJxE1aiyiv2L429BCWfyzAme5sZW8rDb14NeCQHhZbtNqfXhcp2tAnaAT";
+
+    let signature = setup
+        .call_update::<_, String>("sendTransaction", (RpcServices::Mainnet, (), raw_tx))
+        .mock_http_once(MockOutcallBuilder::new(200, r#"{"jsonrpc":"2.0","result":{"context":{"slot":2792},"value":{"blockhash":"EkSnNWid2cvwEVnVx9aBqawnmiCNiDgp3gUdkDPTKN1N","lastValidBlockHeight":3090}},"id":1}"#))
+        .mock_http_once(MockOutcallBuilder::new(200, r#"{"jsonrpc":"2.0","result":"2EanSSkn5cjv9DVKik5gtBkN1wwbV1TAXQQ5yu2RTPGwgrhEywVAQR2veu895uCDzvYwWZe6vD1Bcn8s7r22W17w","id":2}"#))
+        .wait();
+
+    println!("signature: {}", signature);
+}
