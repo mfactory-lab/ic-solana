4e4c50122e090e541edb2e9bd42bb3a91f997b58
tiamo <vk.tiamo@gmail.com>
2024-11-05 04:09:05 +0200

    feat: add `test-utils` crate

--- src/test-utils/src/lib.rs
+++ src/test-utils/src/lib.rs
@@ -1,68 +1,43 @@
 #![allow(dead_code)]
 
-mod mock;
-mod utils;
+pub mod mock;
+pub mod utils;
 
 use std::{marker::PhantomData, sync::Arc, time::Duration};
 
 use candid::{encode_args, utils::ArgumentEncoder, CandidType, Decode, Encode, Principal};
-use ic_canisters_http_types::{HttpRequest, HttpResponse};
 use ic_cdk::api::management_canister::main::CanisterId;
-use ic_solana::{
-    logs::{Log, LogEntry},
-    metrics::Metrics,
-    rpc_client::{RpcResult, RpcServices},
-};
-use ic_solana_rpc::{
-    auth::Auth,
-    state::InitArgs,
-    types::{RegisterProviderArgs, UpdateProviderArgs},
-};
 pub use mock::*;
 use pocket_ic::{
     common::rest::{CanisterHttpResponse, MockCanisterHttpResponse, RawMessageId},
-    CanisterSettings, PocketIc,
+    CanisterSettings, PocketIc, PocketIcBuilder,
 };
 use serde::de::DeserializeOwned;
-
-use crate::setup::utils::{assert_reply, load_wasm_by_name};
-
-const CONTROLLER_ID: Principal = Principal::from_slice(&[0x9d, 0xf7, 0x02]);
-const CALLER_ID: Principal = Principal::from_slice(&[0x9d, 0xf7, 0x01]);
-pub const SOME_CALLER_ID: Principal = Principal::from_slice(&[0x9d, 0xf7, 0x03]);
+use utils::assert_reply;
 
 const INITIAL_CYCLES: u128 = 100_000_000_000_000_000;
+const UPGRADE_TIMEOUT: u64 = 600;
 const MAX_TICKS: usize = 10;
 
-pub const MOCK_RAW_TX: &str ="4hXTCkRzt9WyecNzV1XPgCDfGAZzQKNxLXgynz5QDuWWPSAZBZSHptvWRL3BjCvzUXRdKvHL2b7yGrRQcWyaqsaBCncVG7BFggS8w9snUts67BSh3EqKpXLUm5UMHfD7ZBe9GhARjbNQMLJ1QD3Spr6oMTBU6EhdB4RD8CP2xUxr2u3d6fos36PD98XS6oX8TQjLpsMwncs5DAMiD4nNnR8NBfyghGCWvCVifVwvA8B8TJxE1aiyiv2L429BCWfyzAme5sZW8rDb14NeCQHhZbtNqfXhcp2tAnaAT";
-
 #[derive(Clone)]
-pub struct SolanaRpcSetup {
+pub struct TestSetup {
     pub env: Arc<PocketIc>,
     pub caller: Principal,
     pub controller: Principal,
     pub canister_id: CanisterId,
 }
 
-impl Default for SolanaRpcSetup {
-    fn default() -> Self {
-        Self::new()
-    }
-}
+impl TestSetup {
+    pub fn new<T: CandidType>(wasm: Vec<u8>, args: T) -> Self {
+        let pic = PocketIcBuilder::new()
+            .with_nns_subnet()
+            .with_ii_subnet()
+            .with_application_subnet()
+            .build();
 
-impl SolanaRpcSetup {
-    pub fn new() -> Self {
-        Self::new_with_args(InitArgs {
-            demo: Some(true),
-            managers: Some(vec![CONTROLLER_ID]),
-        })
-    }
-
-    pub fn new_with_args(args: InitArgs) -> Self {
-        let env = Arc::new(PocketIc::new());
-
-        let caller = CALLER_ID;
-        let controller = CONTROLLER_ID;
+        let env = Arc::new(pic);
+        let caller = Self::caller_id();
+        let controller = Self::controller_id();
         let canister_id = env.create_canister_with_settings(
             None,
             Some(CanisterSettings {
@@ -71,12 +46,7 @@ impl SolanaRpcSetup {
             }),
         );
         env.add_cycles(canister_id, INITIAL_CYCLES);
-        env.install_canister(
-            canister_id,
-            Self::wasm_bytes(),
-            Encode!(&args).unwrap(),
-            Some(controller),
-        );
+        env.install_canister(canister_id, wasm, Encode!(&args).unwrap(), Some(controller));
 
         Self {
             env,
@@ -84,6 +54,19 @@ impl SolanaRpcSetup {
             controller,
             canister_id,
         }
+    }
+
+    pub const fn caller_id() -> Principal {
+        Self::principal(0x01)
+    }
+
+    pub const fn controller_id() -> Principal {
+        Self::principal(0x02)
+    }
+
+    /// Creates a principal with the given id
+    pub const fn principal(id: u8) -> Principal {
+        Principal::from_slice(&[0x9d, 0xf7, id])
     }
 
     #[allow(clippy::wrong_self_convention)]
@@ -98,22 +81,13 @@ impl SolanaRpcSetup {
         self
     }
 
-    fn wasm_bytes() -> Vec<u8> {
-        load_wasm_by_name("ic-solana-rpc")
-    }
-
-    pub fn upgrade_canister(&self, args: InitArgs) {
+    pub fn upgrade_canister<A: CandidType>(&self, wasm: Vec<u8>, args: A) {
         self.env.tick();
         // Avoid `CanisterInstallCodeRateLimited` error
-        self.env.advance_time(Duration::from_secs(600));
+        self.env.advance_time(Duration::from_secs(UPGRADE_TIMEOUT));
         self.env.tick();
         self.env
-            .upgrade_canister(
-                self.canister_id,
-                Self::wasm_bytes(),
-                Encode!(&args).unwrap(),
-                Some(self.controller),
-            )
+            .upgrade_canister(self.canister_id, wasm, Encode!(&args).unwrap(), Some(self.controller))
             .expect("Error while upgrading canister");
     }
 
@@ -145,85 +119,17 @@ impl SolanaRpcSetup {
             self.env.advance_time(Duration::from_nanos(1));
         }
     }
-
-    pub fn get_metrics(&self) -> Metrics {
-        self.call_query("getMetrics", ())
-    }
-
-    pub fn http_get_logs(&self, priority: &str) -> Vec<LogEntry> {
-        let request = HttpRequest {
-            method: "".to_string(),
-            url: format!("/logs?priority={priority}"),
-            headers: vec![],
-            body: serde_bytes::ByteBuf::new(),
-        };
-        let response = Decode!(
-            &assert_reply(
-                self.env
-                    .query_call(
-                        self.canister_id,
-                        Principal::anonymous(),
-                        "http_request",
-                        Encode!(&request).unwrap()
-                    )
-                    .expect("failed to get logs")
-            ),
-            HttpResponse
-        )
-        .unwrap();
-        serde_json::from_slice::<Log>(&response.body)
-            .expect("failed to parse logs response")
-            .entries
-    }
-
-    pub fn request(
-        &self,
-        source: RpcServices,
-        method: &str,
-        params: &str,
-        max_response_bytes: u64,
-    ) -> CallFlow<RpcResult<String>> {
-        self.call_update("request", (source, method, params, max_response_bytes))
-    }
-
-    pub fn get_providers(&self) -> Vec<String> {
-        self.call_query("getProviders", ())
-    }
-
-    pub fn register_provider(&self, args: RegisterProviderArgs) -> CallFlow<()> {
-        self.call_update("registerProvider", (args,))
-    }
-
-    pub fn unregister_provider(&self, id: &str) -> CallFlow<bool> {
-        self.call_update("unregisterProvider", (id,))
-    }
-
-    pub fn update_provider(&self, args: UpdateProviderArgs) -> CallFlow<()> {
-        self.call_update("updateProvider", (args,))
-    }
-
-    pub fn get_authorized(&self, auth: Auth) -> Vec<Principal> {
-        self.call_query("getAuthorized", (auth,))
-    }
-
-    pub fn authorize(&self, principal: Principal, auth: Auth) -> CallFlow<bool> {
-        self.call_update("authorize", (principal, auth))
-    }
-
-    pub fn deauthorize(&self, principal: Principal, auth: Auth) -> CallFlow<bool> {
-        self.call_update("deauthorize", (principal, auth))
-    }
 }
 
 pub struct CallFlow<R> {
-    setup: SolanaRpcSetup,
+    setup: TestSetup,
     method: String,
     message_id: RawMessageId,
     phantom: PhantomData<R>,
 }
 
 impl<R: CandidType + DeserializeOwned> CallFlow<R> {
-    pub fn from_update(setup: SolanaRpcSetup, method: &str, input: Vec<u8>) -> Self {
+    pub fn from_update(setup: TestSetup, method: &str, input: Vec<u8>) -> Self {
         let message_id = setup
             .env
             .submit_call(setup.canister_id, setup.caller, method, input.clone())
@@ -231,7 +137,7 @@ impl<R: CandidType + DeserializeOwned> CallFlow<R> {
         Self::new(setup, method, message_id)
     }
 
-    pub fn new(setup: SolanaRpcSetup, method: impl ToString, message_id: RawMessageId) -> Self {
+    pub fn new(setup: TestSetup, method: impl ToString, message_id: RawMessageId) -> Self {
         Self {
             setup,
             method: method.to_string(),
--- src/test-utils/src/utils.rs
+++ src/test-utils/src/utils.rs
@@ -5,7 +5,7 @@ pub fn load_wasm_by_name(name: &str) -> Vec<u8> {
     load_wasm(std::env::var("CARGO_MANIFEST_DIR").unwrap(), name, &[])
 }
 
-pub fn load_wasm_using_env_var(env_var: &str) -> Vec<u8> {
+pub fn load_wasm_by_env_var(env_var: &str) -> Vec<u8> {
     let wasm_path = std::env::var(env_var)
         .unwrap_or_else(|e| panic!("The wasm path must be set using the env variable {} ({})", env_var, e));
     std::fs::read(&wasm_path).unwrap_or_else(|e| {
