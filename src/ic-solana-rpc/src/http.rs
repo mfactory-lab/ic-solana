19f18869dfb359296f4e370cdd32fb9bb7b0ac4a
tiamo <vk.tiamo@gmail.com>
2025-01-06 23:25:33 +0200

    fix: http reqeust cost calculation

--- src/ic-solana-rpc/src/http.rs
+++ src/ic-solana-rpc/src/http.rs
@@ -9,10 +9,9 @@ use ic_solana::{
 
 use crate::{
     constants::{
-        CANISTER_OVERHEAD, COLLATERAL_CYCLES_PER_NODE, HTTP_OUTCALL_REQUEST_BASE_COST,
-        HTTP_OUTCALL_REQUEST_COST_PER_BYTE, HTTP_OUTCALL_REQUEST_PER_NODE_COST, HTTP_OUTCALL_RESPONSE_COST_PER_BYTE,
-        INGRESS_MESSAGE_BYTE_RECEIVED_COST, INGRESS_MESSAGE_RECEIVED_COST, INGRESS_OVERHEAD_BYTES, NODES_IN_SUBNET,
-        RPC_URL_COST_BYTES,
+        COLLATERAL_CYCLES_PER_NODE, DEFAULT_SUBNET_SIZE, HTTP_REQUEST_LINEAR_BASELINE_FEE, HTTP_REQUEST_PER_BYTE_FEE,
+        HTTP_REQUEST_QUADRATIC_BASELINE_FEE, HTTP_RESPONSE_PER_BYTE_FEE, INGRESS_BYTE_RECEPTION_FEE,
+        INGRESS_MESSAGE_RECEPTION_FEE, INGRESS_OVERHEAD_BYTES, NODES_IN_SUBNET, RPC_URL_COST_BYTES,
     },
     providers::find_provider,
     state::read_state,
@@ -65,18 +64,22 @@ fn get_provider_rpc_api(provider_id: &str) -> RpcApi {
         .api()
 }
 
-/// Calculates the cost of sending a JSON-RPC request using HTTP outcalls.
+/// Calculates the baseline cost of sending a request using HTTP outcalls.
+/// The corresponding code in replica:
+/// https://github.com/dfinity/ic/blob/master/rs/cycles_account_manager/src/lib.rs#L1153
 pub fn get_http_request_cost(payload_size_bytes: u64, max_response_bytes: u64) -> u128 {
-    let nodes_in_subnet = NODES_IN_SUBNET as u128;
-    let ingress_bytes = payload_size_bytes as u128 + RPC_URL_COST_BYTES as u128 + INGRESS_OVERHEAD_BYTES;
-    let cost_per_node = INGRESS_MESSAGE_RECEIVED_COST
-        + INGRESS_MESSAGE_BYTE_RECEIVED_COST * ingress_bytes
-        + HTTP_OUTCALL_REQUEST_BASE_COST
-        + HTTP_OUTCALL_REQUEST_PER_NODE_COST * nodes_in_subnet
-        + HTTP_OUTCALL_REQUEST_COST_PER_BYTE * payload_size_bytes as u128
-        + HTTP_OUTCALL_RESPONSE_COST_PER_BYTE * max_response_bytes as u128
-        + CANISTER_OVERHEAD;
-    cost_per_node * nodes_in_subnet
+    let subnet_size = NODES_IN_SUBNET as u128;
+    let request_size = payload_size_bytes as u128;
+    let response_size = max_response_bytes as u128;
+    let ingress_size = request_size + RPC_URL_COST_BYTES as u128 + INGRESS_OVERHEAD_BYTES;
+
+    (INGRESS_MESSAGE_RECEPTION_FEE / DEFAULT_SUBNET_SIZE as u128
+        + INGRESS_BYTE_RECEPTION_FEE / DEFAULT_SUBNET_SIZE as u128 * ingress_size
+        + HTTP_REQUEST_LINEAR_BASELINE_FEE
+        + HTTP_REQUEST_QUADRATIC_BASELINE_FEE * subnet_size
+        + HTTP_REQUEST_PER_BYTE_FEE * request_size
+        + HTTP_RESPONSE_PER_BYTE_FEE * response_size)
+        * subnet_size
 }
 
 /// Calculate the cost + collateral cycles for an HTTP request.
@@ -161,4 +164,40 @@ pub fn serve_logs(request: HttpRequest) -> HttpResponse {
         .header("Content-Type", "application/json; charset=utf-8")
         .with_body_and_content_length(log.serialize_logs(MAX_BODY_SIZE))
         .build()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_request_cost() {
+        let payload = r#"{"jsonrpc":"2.0","method":"sol_getHealth","params":[],"id":1}"#;
+        let base_cost = get_http_request_cost(payload.len() as u64, 1000);
+        let base_cost_10_extra_bytes = get_http_request_cost(payload.len() as u64 + 10, 1000);
+        let estimated_cost_10_extra_bytes = base_cost
+            + 10 * (HTTP_REQUEST_PER_BYTE_FEE + INGRESS_BYTE_RECEPTION_FEE / DEFAULT_SUBNET_SIZE as u128)
+                * NODES_IN_SUBNET as u128;
+        assert_eq!(base_cost_10_extra_bytes, estimated_cost_10_extra_bytes);
+    }
+
+    #[test]
+    fn test_candid_rpc_cost() {
+        assert_eq!(
+            [
+                get_http_request_cost(0, 0),
+                get_http_request_cost(123, 123),
+                get_http_request_cost(123, 4567890),
+                get_http_request_cost(890, 4567890),
+            ],
+            [176350350, 182008596, 124425270996, 124439692130]
+        );
+    }
+
+    #[test]
+    fn test1() {
+        let payload = r#"{"jsonrpc":"2.0","method":"sol_getHealth","params":[],"id":1}"#;
+        let cost = get_http_request_cost(payload.len() as u64, 128u64);
+        println!("cost: {}", cost);
+    }
 }
